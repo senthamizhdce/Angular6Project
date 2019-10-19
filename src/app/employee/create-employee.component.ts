c75dc3441a05d9b26fe47594ee6345efacffde47
senthamizhdce <senthamizh.lcs@gmail.com>
2019-10-20 01:09:30 +0530

    5. Angular form control and form group,AbstractControl abstract base class will have Dirty, Value, Error

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { notStrictEqual } from 'assert';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.css']
 })
+
 export class CreateEmployeeComponent implements OnInit {
+
   employeeForm: FormGroup;
   constructor() { }
 
@@ -19,6 +22,28 @@ export class CreateEmployeeComponent implements OnInit {
 
   onSubmit(): void {
     console.log(this.employeeForm.value);
+    console.log(this.employeeForm); //Check the Log for dirty and more
+    console.log(this.employeeForm.get('fullName').value);
+    console.log(this.employeeForm.controls.fullName.value);     
   }
 
 }
+
+// Notes
+// -----
+// 1. When working with reactive forms we create instances of FormControl and FormGroup classes to create a form model.
+// 2. To bind an HTML <form> tag in the template to the FromGroup instance in the component class, we use formGroup directive
+// 3. To bind an  HTML <input> element in the template to the FormControl instance in the component class, we use formControlName directive
+// 4. formGroup and formControlName directives are provided by the ReactiveFormsModule
+// 5. Both FormControl and FormGroup classes inherit from AbstractControl base class
+// 6. The AbstractControl class has properties that help us track both FormControl and FormGroup value and state
+
+// AbstractControl - The following are some of the useful properties provided by the AbstractControl class
+// value
+// errors
+// valid
+// invalid
+// dirty
+// pristine
+// touched
+// untouched
